c2d02d73501366acf6c0db9effc2ba3c264418a6
HironoriSato <maza.hs5360@gmail.com>
2020-02-13 00:24:36 +0900

    [add]home画面にtopicsへのリンクを追加, テストの作成

--- boards/tests.py
+++ boards/tests.py
@@ -5,16 +5,24 @@ from .models import Board
 
 
 class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     # ステータスコードが200を返すか判定する
     def test_home_view_status_code(self):
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     # 正しいviewを返すか判定する
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+    # レスポンス内にTopicsへのリンクを含むか判定
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
 
 
 class BoardTopicsTests(TestCase):
