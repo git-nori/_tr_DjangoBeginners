9dfcf8ab3d1b45f2e1d6a5f6a1c9a57b02b9cd2d
HironoriSato <maza.hs5360@gmail.com>
2020-02-12 23:42:16 +0900

    [add]home画面のテストを作成

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import home
 
-# Create your tests here.
+
+class HomeTests(TestCase):
+    # ステータスコードが200を返すか判定する
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    # 正しいviewを返すか判定する
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
