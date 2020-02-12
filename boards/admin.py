3ca1e0af093de49bbd364550c0178fb402982bc3
HironoriSato <maza.hs5360@gmail.com>
2020-02-12 21:12:02 +0900

    [add]modelsにBoard, Topic, Postを追加

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Board, Topic, Post
 
-# Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
