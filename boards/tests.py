20eefcb528378110a2b0a7d9eb3d6becd04cf274
HironoriSato <maza.hs5360@gmail.com>
2020-02-13 00:13:33 +0900

    [add]BoardTopicsのテストを作成

--- boards/tests.py
+++ boards/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from .views import home
+from .views import home, board_topics
+from .models import Board
 
 
 class HomeTests(TestCase):
@@ -14,3 +15,25 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+
+    # ステータスコードが200を返すか判定する
+    def test_board_topics_view_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    # ステータスコードが404を返すか判定する
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    # 正しいviewを返すか判定する
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/boards/1')
+        self.assertEquals(view.func, board_topics)
