ddc1fdbf6eea2b47416a67ec5eb32f97c7a39a1b
HironoriSato <maza.hs5360@gmail.com>
2020-02-15 10:18:37 +0900

    [add]board_topics画面のPOSTリクエストに対してのテストを作成

--- boards/tests.py
+++ boards/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from .views import home, board_topics, new_topic
-from .models import Board
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
 
 
 class HomeTests(TestCase):
@@ -61,6 +62,7 @@ class BoardTopicsTests(TestCase):
 class NewTopicTests(TestCase):
     def setUp(self):
         Board.objects.create(name='Django', description={'Django board'})
+        User.objects.create_user(username='john', email='john@doe.com', password='123')
 
     # ステータスコードが200を返すか判定する
     def test_new_topic_view_status_code(self):
@@ -85,3 +87,37 @@ class NewTopicTests(TestCase):
         response = self.client.get(new_topic_url)
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         self.assertContains(response, 'href="{0}"'.format(board_topics_url))
+
+    # リクエストにcsrfトークンを含むか判定
+    def test_csrf(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    # POSTリクエストが成功した場合、データが作成されているか判定
+    def test_new_topic_valid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': 'Test title',
+            'message': 'Test message'
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
+    # 空のデータを送った場合、ステータスコードが200を返すか判定する
+    def test_new_topic_invalid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+
+    # バリデーションエラーが発生するデータ送った場合、ステータスコードが200を返すか判定する
+    def test_new_topic_invalid_post_data_empty_fields(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': '',
+            'message': ''
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
