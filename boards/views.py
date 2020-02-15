b2bbb99a3a8da63f6fcd7f1fbb789aee0c69b3c1
HironoriSato <maza.hs5360@gmail.com>
2020-02-15 10:18:13 +0900

    [mod]board_topics画面にFormを追加

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
+
+from boards.forms import NewTopicForm
 from .models import Board, Topic, Post
 from django.contrib.auth.models import User
 
@@ -15,25 +17,23 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
 
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            # Save Topic
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
 
-        user = User.objects.first()
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        return redirect('board_topics', pk=board.pk)
-
-    return render(request, 'new_topic.html', {'board': board})
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
