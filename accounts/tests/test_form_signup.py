d4323f79b431c043d129da748806593ec582a5c6
HironoriSato <maza.hs5360@gmail.com>
2020-02-16 22:41:37 +0900

    [add]signup画面に使用するSignUpFormのテストを作成

---
+++ accounts/tests/test_form_signup.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from ..forms import SignUpForm
+
+
+class SignUpFormTest(TestCase):
+
+    # フォームの項目が正しいか確認する
+    def test_form_has_fields(self):
+        form = SignUpForm()
+        expected = ['username', 'email', 'password1', 'password2']
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
