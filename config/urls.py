4c4fdb3e304ad7d01d9d1d3723be34a4a90a5d95
HironoriSato <maza.hs5360@gmail.com>
2020-02-17 02:53:38 +0900

    [add]password_change_done画面を作成

--- config/urls.py
+++ config/urls.py
@@ -44,4 +44,7 @@ urlpatterns = [
     path('settings/password/', auth_views.PasswordChangeView.as_view(
         template_name='password_change.html'
     ), name='password_change'),
+    path('settings/password/done', auth_views.PasswordChangeDoneView.as_view(
+        template_name='password_change_done.html'
+    ), name='password_change_done'),
 ]
