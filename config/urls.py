e1f9869e73d61e6e68c322c03808ed5d33d27546
HironoriSato <maza.hs5360@gmail.com>
2020-02-16 23:23:22 +0900

    [add]logout時の処理を追加

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from boards import views
 from accounts import views as account_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('boards/<int:pk>', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new', views.new_topic, name='new_topic'),
     path('signup/', account_views.signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
