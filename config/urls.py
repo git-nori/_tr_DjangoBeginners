1045956a823c9a9d2b72379576065132e9abbbc5
HironoriSato <maza.hs5360@gmail.com>
2020-02-16 23:43:04 +0900

    [add]login画面を作成

--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('boards/<int:pk>/new', views.new_topic, name='new_topic'),
     path('signup/', account_views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
