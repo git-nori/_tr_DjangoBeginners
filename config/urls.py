dfd232c325170d665d12a6de8a98b52d724eec03
HironoriSato <maza.hs5360@gmail.com>
2020-02-17 02:53:16 +0900

    [add]password_change画面を作成

--- config/urls.py
+++ config/urls.py
@@ -41,4 +41,7 @@ urlpatterns = [
     path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(
         template_name='password_reset_complete.html'
     ), name='password_reset_complete'),
+    path('settings/password/', auth_views.PasswordChangeView.as_view(
+        template_name='password_change.html'
+    ), name='password_change'),
 ]
