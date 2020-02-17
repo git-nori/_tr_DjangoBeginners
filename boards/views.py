c9732f368ca64df2edcbb9438601e13d511218cc
HironoriSato <maza.hs5360@gmail.com>
2020-02-18 00:30:23 +0900

    [add]reply_topic画面を作成

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
-from boards.forms import NewTopicForm
+from boards.forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -43,3 +43,20 @@ def new_topic(request, pk):
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
     return render(request, 'topic_posts.html', {'topic': topic})
+
+
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+    else:
+        form = PostForm()
+
+    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
