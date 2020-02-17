f4062efc6cf15745cb05de4a26ba3c1f0382e049
HironoriSato <maza.hs5360@gmail.com>
2020-02-17 19:19:08 +0900

    [add]ログイン後にリダイレクトする機能のテストケースを作成

--- boards/tests/test_view_new_topic.py
+++ boards/tests/test_view_new_topic.py
@@ -7,6 +7,17 @@ from boards.models import Board, Topic, Post
 from django.contrib.auth.models import User
 
 
+class LoginRequiredNewTopicTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+        self.url = reverse('new_topic', kwargs={'pk': 1})
+        self.response = self.client.get(self.url)
+
+    def test_redirection(self):
+        login_url = reverse('login')
+        self.assertRedirects(self.response, '{login_url}?next={url}'.format(login_url=login_url, url=self.url))
+
+
 class NewTopicTests(TestCase):
     def setUp(self):
         username = 'john'
