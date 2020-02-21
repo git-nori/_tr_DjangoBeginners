7f2ebba1835c8354a46f89a8b49c13674306cc4c
HironoriSato <maza.hs5360@gmail.com>
2020-02-21 22:10:38 +0900

    [add]topic画面に表示するpostのページ数を出力するメソッドを定義

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,5 @@
+import math
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -27,6 +29,22 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.subject
+
+    def get_page_count(self):
+        count = self.posts.count()
+        pages = count / 2
+        return math.ceil(pages)
+
+    def has_many_pages(self, count=None):
+        if count is None:
+            count = self.get_page_count()
+        return count > 6
+
+    def get_page_range(self):
+        count = self.posts.count()
+        if self.has_many_pages(count):
+            return range(1, 5)
+        return range(1, count + 1)
 
 
 class Post(models.Model):
